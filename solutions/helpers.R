1d1998f8c846502883ba8b6983763a4d3e9a4356
Christian B. Knudsen <cbk@kb.dk>
2025-09-04 13:39:14 +0200

    hexagonal generator, pentagonal test

--- solutions/helpers.R
+++ solutions/helpers.R
@@ -7,3 +7,13 @@ is_triangle <- function(x) {
 triangle <- function(x){
   (x*(x+1))/2
 }
+
+hexagonal <-function(x){
+  2*x^2 - x
+}
+
+is_pentagonal <- function(x){
+  if(x<1) return(FALSE)
+  s <- (sqrt(24*x +1)+1)/6
+  s == floor(s) # We probably need some way of controlling the precision
+}
